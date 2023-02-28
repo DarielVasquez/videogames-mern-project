4364e3aa7a80575fc731da762e7741bed67bbc63
Dariel Vasquez <darielalejandro10@gmail.com>
2023-02-28 16:07:36 -0600

    another change

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,14 @@
-import { combineReducers } from 'redux';
-import fetchProductsReducer from './fetchProductsReducer';
-import fetchSingleProductReducer from './fetchSingleProductReducer';
+import { combineReducers } from "redux";
+import fetchProductsReducer from "./fetchProductsReducer";
+import fetchSingleProductReducer from "./fetchSingleProductReducer";
+import fetchMainProductReducer from "./fetchMainProductReducer";
+import fetchQueryReducer from "./fetchQueryReducer";
 
 const allReducers = combineReducers({
-    fetchProducts: fetchProductsReducer,
-    fetchSingleProduct: fetchSingleProductReducer
+  fetchProducts: fetchProductsReducer,
+  fetchSingleProduct: fetchSingleProductReducer,
+  fetchMainProduct: fetchMainProductReducer,
+  fetchQuery: fetchQueryReducer,
 });
 
 export default allReducers;
